35d8d75bf9c3f3e2dc7b7bca3858c2f5e416949b
Symuhmd <symuhmmd114@gmail.com>
2024-06-01 17:48:09 +0800

    Update BuckshotRoullete2Bit.cpp
    no class

--- BuckshotRoullete2Bit.cpp
+++ BuckshotRoullete2Bit.cpp
@@ -3,40 +3,50 @@
 #include <algorithm>
 #include <ctime>
 #include <cstdlib>
-
 using namespace std;
 
-enum class Buff {
-    Regen,
-    Lock,
-    Peek,
-    DoubleDamage,
-};
-
 struct BuffInfo {
-    Buff buff;
+    string buff;
     bool used;
 };
 
 void initializeGame(int& playerHealth, int& computerHealth, vector<bool>& bullets, vector<BuffInfo>& Buffs) {
 
-    vector<Buff> allBuffs = { Buff::Regen, Buff::Lock, Buff::Peek, Buff::DoubleDamage };
-    random_shuffle(allBuffs.begin(), allBuffs.end());
+    vector<string> allBuffs = { "Regen", "Lock", "Peek", "DoubleDamage" };
 
-    // Push the first two shuffled buffs into playerBuffs
+    // Seed the random number generator with the current time
+    srand(time(0));
+
+    // Use a vector to store unique indices
+    vector<int> usedIndices;
+
+    // Push the first two random buffs into Buffs
     for (int i = 0; i < 2; i++) {
-        Buffs.push_back({ allBuffs[i], false });
-    }
+        int buffindex;
+        do {
+            buffindex = rand() % 4; // Random index between 0 and 3
+        } while (find(usedIndices.begin(), usedIndices.end(), buffindex) != usedIndices.end());
 
+        // Add the selected index to usedIndices to avoid duplicates
+        usedIndices.push_back(buffindex);
+
+        // Push the selected buff into Buffs
+        Buffs.push_back({ allBuffs[buffindex], false });
+    }
+    // Initialize player and computer health
     playerHealth = 3;
     computerHealth = 3;
 
+    // Initialize bullets
     bullets = { true, true, true, true, true, true, false, false, false, false };
-    srand(static_cast<unsigned int>(std::time(0)));
+
+    // Shuffle bullets
     random_shuffle(bullets.begin(), bullets.end());
 }
 
+
 bool shootBullet(vector<bool>& bullets) {
+
     if (bullets.empty()) {
         cout << "No more bullets left!" << endl;
         return false;
@@ -47,31 +57,35 @@ bool shootBullet(vector<bool>& bullets) {
     return bullet;
 }
 
-void applyBuff(string Player, Buff buff, int& playerHealth, int& computerHealth, bool& turn, bool& isLive, int& damage) {
-    switch (buff) {
-    case Buff::Regen:
+void applyBuff(string Player, string buff, int& playerHealth, int& computerHealth, bool& turn, bool& isLive, int& damage) {
+
+    if (buff == "Regen")
+    {
         playerHealth++;
         cout << Player << "used Regen buff." << Player << "health is now " << playerHealth << endl;
-        break;
-    case Buff::Lock:
+    }
+    else if (buff == "Lock")
+    {
         turn = true;
         cout << Player << "used Lock buff.Opponent's turn will be skip." << endl;
-        break;
-    case Buff::Peek:
+
+    }
+    else if (buff == "Peek")
+    {
         if (isLive)
             cout << Player << "used Peek buff.The bullet is live." << endl;
         else
             cout << Player << "used Peek buff.The bullet is blank." << endl;
-        break;
-    case Buff::DoubleDamage:
+    }
+    else if (buff == "DoubleDamage")
+    {
         cout << Player << "used Double Damage buff.Double damage will apply on shoot." << endl;
         damage = 2;
-        break;
-        // Implement other buffs
     }
 }
 
 void playerTurn(int& playerHealth, int& computerHealth, vector<bool>& bullets, vector<BuffInfo>& playerBuffs, bool& CPUTurn, int& damage) {
+
     cout << "\nYour remaining health: " << playerHealth << " | Computer's remaining health: " << computerHealth << endl;
     bool isLive = shootBullet(bullets);
 
@@ -86,22 +100,30 @@ void playerTurn(int& playerHealth, int& computerHealth, vector<bool>& bullets, v
         {
             for (size_t i = 0; i < playerBuffs.size(); ++i) {
                 if (!playerBuffs[i].used) {
+
                     cout << "(" << i + 1 << ") ";
-                    switch (playerBuffs[i].buff) {
-                    case Buff::Regen:
+
+                    if (playerBuffs[i].buff == "Regen")
+                    {
                         cout << "Regen ";
-                        break;
-                    case Buff::Lock:
-                        cout << "Lock ";
-                        break;
-                    case Buff::Peek:
-                        cout << "Peek ";
-                        break;
-                    case Buff::DoubleDamage:
-                        cout << "Double Damage ";
-                        break;
-                        // Implement other buffs
+
                     }
+                    else if (playerBuffs[i].buff == "Lock")
+                    {
+                        cout << "Lock ";
+
+                    }
+                    else if (playerBuffs[i].buff == "Peek")
+                    {
+                        cout << "Peek ";
+
+                    }
+                    else if (playerBuffs[i].buff == "DoubleDamage")
+                    {
+                        cout << "Double Damage ";
+
+                    }
+
                 }
             }
         }
@@ -110,9 +132,10 @@ void playerTurn(int& playerHealth, int& computerHealth, vector<bool>& bullets, v
         }
         cout << endl;
         cin >> index;
+        int buffIndex = index - 1;
 
         if (index > 0 && index <= playerBuffs.size() && !playerBuffs[index - 1].used) {
-            applyBuff("You ", playerBuffs[index - 1].buff, playerHealth, computerHealth, CPUTurn, isLive, damage);
+            applyBuff("You ", playerBuffs[buffIndex].buff, playerHealth, computerHealth, CPUTurn, isLive, damage);
             playerBuffs[index - 1].used = true;
         }
         else {
@@ -160,27 +183,29 @@ void computerTurn(int& playerHealth, int& computerHealth, vector<bool>& bullets,
         for (size_t i = 0; i < computerBuffs.size(); ++i) {
             if (!computerBuffs[i].used) {
                 cout << "(" << i + 1 << ") ";
-                switch (computerBuffs[i].buff) {
-                case Buff::Regen:
+
+                if (computerBuffs[i].buff == "Regen")
+                {
                     cout << "Regen ";
-                    break;
-                case Buff::Lock:
+                }
+                else if (computerBuffs[i].buff == "Lock")
+                {
                     cout << "Lock ";
-                    break;
-                case Buff::Peek:
+                }
+                else if (computerBuffs[i].buff == "Peek")
+                {
                     cout << "Peek ";
-                    break;
-                case Buff::DoubleDamage:
+                }
+                else if (computerBuffs[i].buff == "DoubleDamage")
+                {
                     cout << "Double Damage ";
-                    break;
-                    // Implement other buffs
                 }
             }
         }
         cout << endl;
-
+        int buffIndex = index - 1;
         if (index > 0 && index <= computerBuffs.size() && !computerBuffs[index - 1].used) {
-            applyBuff("Computer ", computerBuffs[index - 1].buff, computerHealth, computerHealth, isPlayerTurnSkip, isLive, damage);
+            applyBuff("Computer ", computerBuffs[buffIndex].buff, computerHealth, computerHealth, isPlayerTurnSkip, isLive, damage);
             computerBuffs[index - 1].used = true;
         }
         else {
@@ -223,7 +248,7 @@ void computerTurn(int& playerHealth, int& computerHealth, vector<bool>& bullets,
 
 int main() {
 
-    int playerHealth, computerHealth, damage = 1;
+    int playerHealth, computerHealth, damage;
     vector<bool> bullets;
     vector<BuffInfo> playerBuffs, computerBuffs;
     bool isPlayerTurnSkip = false, isCPUTurnSkip = false;
@@ -233,56 +258,60 @@ int main() {
     cout << "--------------------------------" << endl;
     cout << "At the beginning of the game, 10 bullets are loaded into the gun.\n" << "6 bullets are live, and 4 are blank." << endl;
 
-    initializeGame(playerHealth, computerHealth, bullets, playerBuffs);
-    initializeGame(playerHealth, computerHealth, bullets, computerBuffs);
+    while (true) {
+        initializeGame(playerHealth, computerHealth, bullets, playerBuffs);
+        initializeGame(playerHealth, computerHealth, bullets, computerBuffs);
 
-    //playerBuffs.push_back({ Buff::Regen, false });
-    //playerBuffs.push_back({ Buff::Lock, false });
-    //playerBuffs.push_back({ Buff::Peek, false });
-    //playerBuffs.push_back({ Buff::DoubleDamage, false });
-    // Add other buffs for player
+        cout << "Buff given " << playerBuffs[0].buff << " and " << playerBuffs[1].buff << ".\n";
+        damage = 1;
+        while (playerHealth > 0 && computerHealth > 0 && !bullets.empty()) {
 
-    //computerBuffs.push_back({ Buff::Regen, false });
-    //computerBuffs.push_back({ Buff::Lock, false });
-    //computerBuffs.push_back({ Buff::Peek, false });
-    //computerBuffs.push_back({ Buff::DoubleDamage, false });
-    // Add other buffs for computer
+            isCPUTurnSkip = false;
+            if (!isPlayerTurnSkip) {
+                playerTurn(playerHealth, computerHealth, bullets, playerBuffs, isCPUTurnSkip, damage);
+            }
+            else {
+                cout << "\nYour's turn is skipped due to Lock buff!" << endl;
+            }
 
-    while (playerHealth > 0 && computerHealth > 0 && !bullets.empty()) {
+            if (playerHealth <= 0 || computerHealth <= 0 || bullets.empty()) {
+                break;
+            }
 
-        isCPUTurnSkip = false;
-        if (!isPlayerTurnSkip) {
-            playerTurn(playerHealth, computerHealth, bullets, playerBuffs, isCPUTurnSkip, damage);
+            isPlayerTurnSkip = false;
+            if (!isCPUTurnSkip) {
+                computerTurn(playerHealth, computerHealth, bullets, computerBuffs, isPlayerTurnSkip, damage);
+            }
+            else {
+                cout << "Computer's turn is skipped due to Lock buff!" << endl;
+            }
+
+            damage = 1;
+        }
+
+        cout << endl;
+        if (playerHealth <= 0) {
+            cout << "You lost! The computer wins!" << endl;
+        }
+        else if (computerHealth <= 0) {
+            cout << "You win! The computer is defeated!" << endl;
         }
         else {
-            cout << "Your's turn is skipped due to Lock buff!" << endl;
+            cout << "No more bullets left! It's a draw!" << endl;
         }
 
-        if (playerHealth <= 0 || computerHealth <= 0 || bullets.empty()) {
+        int repeat;
+        cout << "Do you want to play again ? (1) yes (2) no"; 
+        cin >> repeat;
+
+        if (repeat == 1)
+        {
+            cout << endl;
+        }
+        else
+        {
             break;
         }
-
-        isPlayerTurnSkip = false;
-        if (!isCPUTurnSkip) {
-            computerTurn(playerHealth, computerHealth, bullets, computerBuffs, isPlayerTurnSkip, damage);
-        }
-        else {
-            cout << "Computer's turn is skipped due to Lock buff!" << endl;
-        }
-
-        damage = 1;
     }
-
-    cout << endl;
-    if (playerHealth <= 0) {
-        cout << "You lost! The computer wins!" << endl;
-    }
-    else if (computerHealth <= 0) {
-        cout << "You win! The computer is defeated!" << endl;
-    }
-    else {
-        cout << "No more bullets left! It's a draw!" << endl;
-    }
-
     return 0;
 }
